ced6e51985a9a960b90be48fb924092fd6eff617
C. Jordan Ball III <cjordanball@gmail.com>
2017-06-13 10:37:38 -0400

    update signup to handle additional email field

--- models/users.js
+++ models/users.js
@@ -11,6 +11,10 @@ const UserSchema = new Schema(
 			required: [true, 'Please enter username.'],
 			lowercase: true
 		},
+		email: {
+			type: String,
+			lowercase: true
+		},
 		password: {
 			type: String,
 			required: [true, 'Please enter a password.']
