bda0739d0aac8a53597966928427ecd9c5f9e980
C. Jordan Ball III <cjordanball@gmail.com>
2017-06-14 17:19:08 -0400

    add new auth routes

--- router.js
+++ router.js
@@ -8,7 +8,7 @@ const requireSignin = passport.authenticate('local', { session: false });
 module.exports = (app) => {
 	app.get('/auth/test', AuthController.toast);
 	app.get('/test', AuthController.plaintest);
-	app.post('/signup', AuthController.signup);
+	app.post('/auth/signup', AuthController.signup);
 
-	app.post('/signin', requireSignin, AuthController.signin);
+	app.post('/auth/signin', requireSignin, AuthController.signin);
 };
